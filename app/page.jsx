81106c1821c807870bff56bb2c432f73e8b79388
Sayan <mahantosayan@gmail.com>
2025-08-17 12:12:25 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
+// components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+
 const page = () => {
   return (
     <section className="h-full">
@@ -16,8 +20,26 @@ const page = () => {
               I excel at crafting elegant digital experiences and I am
               proficient in varios progeamming languages and technologies.
             </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2 hover:transition-all duration-500"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
           </div>
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
